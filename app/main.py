1be5511913963aca43a722a8cb510f50053392c9
Siraj Ahmed <blu.knite@gmail.com>
2026-08-25 06:58:01 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,13 +2,14 @@ import sys
 
 
 def main():
-    while True:
-        repl()
+    while repl():
+        pass
 
 def repl():
     sys.stdout.write('$ ')
     command = input()
     sys.stdout.write(f'{command}: command not found\n')
+    return True
 
 if __name__ == "__main__":
     main()
