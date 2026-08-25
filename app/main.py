32f0d21367e09c6fba4b8cc71cd4fc9ca2dad2f9
Siraj Ahmed <blu.knite@gmail.com>
2026-08-25 07:18:12 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -11,16 +11,33 @@ def repl():
     return execute_command(command)
 
 def execute_command(command: str) -> bool:
-    if command == 'exit':
-        return False
-    elif command.startswith('echo'):
-        return echo(command)
+    args = command.split(' ')
+    if args[0] in built_ins.keys():
+        return built_ins[args[0]](args[1:])
     print(f'{command}: command not found')
     return True
 
-def echo(command: str) -> bool:
-    print(command[5:])
+def exit(args: list[str]) -> bool:
+    return False
+
+def echo(args: list[[str]]) -> bool:
+    for arg in args:
+        sys.stdout.write(f'{arg} ')
+    sys.stdout.write('\n')
     return True
+
+def type(args: list[str]) -> bool:
+    if args[0] in built_ins.keys():
+        print(f'{args[0]} is a shell builtin')
+    else:
+        print(f'{args[0]}: not found')
+    return True
+
+built_ins = {
+    'echo': echo,
+    'exit': exit,
+    'type': type
+}
 
 if __name__ == "__main__":
     main()
