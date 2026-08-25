d1bd63fe834b7caed5339f680392d85123365f6b
Siraj Ahmed <blu.knite@gmail.com>
2026-08-25 07:09:40 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,9 +8,18 @@ def main():
 def repl():
     sys.stdout.write('$ ')
     command = input()
+    return execute_command(command)
+
+def execute_command(command: str) -> bool:
     if command == 'exit':
         return False
-    sys.stdout.write(f'{command}: command not found\n')
+    elif command.startswith('echo'):
+        return echo(command)
+    print(f'{command}: command not found')
+    return True
+
+def echo(command: str) -> bool:
+    print(command[5:])
     return True
 
 if __name__ == "__main__":
